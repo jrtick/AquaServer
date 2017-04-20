bcf1711b530e92aa30bf9d2bd3aa2050fe767ac4
EC2 Default User <ec2-user@ip-172-31-35-7.us-east-2.compute.internal>
2017-04-20 03:39:22 +0000

    added a gitignore for node packages and updated .json

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-var passwords = {};
-passwords["Jordan"] = "password1234";
-
 /* load credentials */
 var thingName = "AquaOS";
 var tableName = "TempWatch";
